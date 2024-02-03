8538b84de53c478c8cf3deb4f5d4a2cbbcee695d
Gerson Bernardo <gbds25@gmail.com>
2024-02-03 15:24:04 -0300

    Criar função

--- js/loadPartials.js
+++ js/loadPartials.js
@@ -1,16 +1,16 @@
 function loadPartialHTML_(partial){
-    const htmlServ = HtmlService.createTemplateFromFile(partial);
-    return htmlServ.evaluate().getContent();
-  }
+  const htmlServ = HtmlService.createTemplateFromFile(partial);
+  return htmlServ.evaluate().getContent();
+}
+
+function loadSearchView(){
+
+  return loadPartialHTML_("search");
+
+}
+
+function loadAddCustomerView(){
+
+  return loadPartialHTML_("addcustomer");
   
-  function loadSearchView(){
-  
-    return loadPartialHTML_("search");
-  
-  }
-  
-  function loadAddCustomerView(){
-  
-    return loadPartialHTML_("addcustomers");
-    
-  }
+}
